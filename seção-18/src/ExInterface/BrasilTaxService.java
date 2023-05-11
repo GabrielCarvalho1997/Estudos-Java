83a11586fb9ae4b3f3dbbdba672dda34dfd157a7
Gabriel Carvalho <gabrielcoelhoneves@hotmail.com>
2023-05-11 15:58:34 -0300

    Exercício Interface

---
+++ seção-18/src/ExInterface/BrasilTaxService.java
@@ -0,0 +1,13 @@
+package ExInterface;
+
+public class BrasilTaxService {
+
+    public double tax(Double amount) {
+        if (amount <= 100.0) {
+            return amount * 0.2;
+        } else {
+            return amount * 0.15;
+        }
+    }
+
+}
