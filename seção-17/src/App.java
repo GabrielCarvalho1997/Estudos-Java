80a27b528902ab7173f7a3fc01892a49120ff6e1
Gabriel Carvalho <gabrielcoelhoneves@hotmail.com>
2023-05-11 11:13:50 -0300

    read files

---
+++ seção-17/src/App.java
@@ -0,0 +1,22 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        File file = new File("C:\\Users\\supor\\Desktop\\Nova pasta\\Estudos-Java\\seção-17\\in.txt");
+        Scanner sc = null;
+        try {
+            sc = new Scanner(file);
+            while (sc.hasNextLine()) {
+                System.out.println(sc.nextLine());
+            }
+        } catch (IOException e) {
+            System.out.print("Error: " + e.getMessage());
+        } finally {
+            if (sc != null) {
+                sc.close();
+            }
+        }
+    }
+}
